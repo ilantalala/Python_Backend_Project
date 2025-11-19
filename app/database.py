ea5d5aaeb93f81067e306dc191211c1cf007f57b
ilantalala <ilant200@gmail.com>
2025-11-19 16:46:45 +0200

    fixed the URL database

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL=f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL='postgresql://fastapi_database_8lk5_user:YURVuJwhn2RPlg6zcje1bjBlrSU2pjtq@dpg-d44vanngi27c73ajqjt0-a/fastapi_database_8lk5'
 
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
